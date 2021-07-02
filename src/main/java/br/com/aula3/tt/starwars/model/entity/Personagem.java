a01495b55b911396ec1c7765d8faf0c326cb6b3d
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-02 18:43:05 -0300

    criação da entidade personagem

---
+++ src/main/java/br/com/aula3/tt/starwars/model/entity/Personagem.java
@@ -0,0 +1,70 @@
+package br.com.aula3.tt.starwars.model.entity;
+
+public class Personagem {
+    private String name;
+    private Integer height;
+    private Integer mass;
+    private String hair_color;
+    private String skin_color;
+    private String eye_color;
+    private String birth_year;
+    private String gender;
+    private String homeworld;
+    private String species;
+
+    public Personagem() {
+    }
+
+    public Personagem(String name, Integer height, Integer mass, String hair_color, String skin_color, String eye_color, String birth_year, String gender, String homeworld, String species) {
+        this.name = name;
+        this.height = height;
+        this.mass = mass;
+        this.hair_color = hair_color;
+        this.skin_color = skin_color;
+        this.eye_color = eye_color;
+        this.birth_year = birth_year;
+        this.gender = gender;
+        this.homeworld = homeworld;
+        this.species = species;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public Integer getMass() {
+        return mass;
+    }
+
+    public String getHair_color() {
+        return hair_color;
+    }
+
+    public String getSkin_color() {
+        return skin_color;
+    }
+
+    public String getEye_color() {
+        return eye_color;
+    }
+
+    public String getBirth_year() {
+        return birth_year;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getHomeworld() {
+        return homeworld;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+}
