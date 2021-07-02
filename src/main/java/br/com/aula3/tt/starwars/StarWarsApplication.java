f53761102bc09dcd719c3bc70bc228dcabd24d58
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-02 18:36:35 -0300

    Inicialização do projeto spring boot

---
+++ src/main/java/br/com/aula3/tt/starwars/StarWarsApplication.java
@@ -0,0 +1,13 @@
+package br.com.aula3.tt.starwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarWarsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/aula3/tt/starwars/StarWarsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aula3.tt.starwars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
