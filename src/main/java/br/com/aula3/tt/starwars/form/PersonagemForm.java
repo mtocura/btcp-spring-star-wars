53f588920f26db1d160693100305846bf6e410b0
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-02 18:45:20 -0300

    criação do form para personagem

---
+++ src/main/java/br/com/aula3/tt/starwars/form/PersonagemForm.java
@@ -0,0 +1,16 @@
+package br.com.aula3.tt.starwars.form;
+
+public class PersonagemForm {
+    private String name;
+
+    public PersonagemForm() {
+    }
+
+    public PersonagemForm(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
