16ced6cf1045ec9b64c75bf6e049dbd8de258bfc
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-05 07:55:13 -0300

    minor fix no nome do arquivo json

--- src/main/java/br/com/aula3/tt/starwars/model/repository/PersonagemRepository.java
+++ src/main/java/br/com/aula3/tt/starwars/model/repository/PersonagemRepository.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @Repository
 public class PersonagemRepository {
-    private static final File FILE = new File("star-wars.json");
+    private static final File FILE = new File("starwars.json");
 
     @Autowired
     private ObjectMapper mapper;
