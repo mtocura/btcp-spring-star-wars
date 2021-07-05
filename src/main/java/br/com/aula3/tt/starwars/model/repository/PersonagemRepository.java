f52f9c3403b889611144b16bb1a3edc097845d11
Mauricio Tocura <mauricio.tocura@mercadolivre.com>
2021-07-05 07:47:09 -0300

    Criação do repository

---
+++ src/main/java/br/com/aula3/tt/starwars/model/repository/PersonagemRepository.java
@@ -0,0 +1,48 @@
+package br.com.aula3.tt.starwars.model.repository;
+
+import br.com.aula3.tt.starwars.model.entity.Personagem;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class PersonagemRepository {
+    private static final File FILE = new File("clientes.json");
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    public List<Personagem> getAll(){
+        List<Personagem> personagens = new ArrayList<>();
+        try {
+            FileInputStream is = new FileInputStream(FILE);
+            TypeReference<List<Personagem>> typeReference = new TypeReference<List<Personagem>>() {};
+            personagens = mapper.readValue(is, typeReference);
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return personagens;
+    }
+
+    public List<String> nomePersonagem(String nome) {
+        List<Personagem> personagens = getAll();
+        List<String> nomes = new ArrayList<>();
+
+        for (Personagem personagem : personagens) {
+            if(personagem.getName().toLowerCase().contains(nome)) {
+                nomes.add(personagem.getName());
+            }
+        }
+
+        return nomes;
+    }
+}
